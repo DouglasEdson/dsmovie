8b3486ff767e9b22ef4fa37f12eca547001a1d35
DouglasEdson <douglas22@live.com.pt>
2022-01-15 10:54:59 -0300

    useState, useEffect

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,10 +7,10 @@ function Navbar() {
         <nav className="container">
             <div className="dsmovie-nav-content">
                 <h1>DSMovie</h1>
-                <a href="https://github.com/DionatanLeao" target="_blank" rel="noreferrer">
+                <a href="https://github.com/DouglasEdson" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/DionatanLeao</p>
+                        <p className="dsmovie-contact-link">/DouglasEdson</p>
                     </div>
                 </a>
             </div>
